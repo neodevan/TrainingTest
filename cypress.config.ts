fdaa381ba89466fa7af6ab0dfc3640dd2942e6eb
neodevan <itsmevipinmp5@gmail.com>
2022-12-21 14:38:23 +0000

    Added Record key to env

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: 'ss7ape',
   e2e: {
+    env:{
+      RECORD_KEY: "690a20b7-73e9-456d-b052-72c5663d1f7a"
+    },
     chromeWebSecurity:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here

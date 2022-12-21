4778e614d7bd52caff45ba22839b56801add7d91
neodevan <itsmevipinmp5@gmail.com>
2022-12-21 16:00:22 +0000

    For Exercise multi-domain

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'ss7ape',
+  projectId: "ss7ape",
   e2e: {
-    env:{
-      RECORD_KEY: "690a20b7-73e9-456d-b052-72c5663d1f7a"
+    env: {
+      RECORD_KEY: "690a20b7-73e9-456d-b052-72c5663d1f7a",
     },
-    chromeWebSecurity:false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      let value:any;
+
+      on("task", {
+        log(message) {
+          console.log(message);
+
+          return null;
+        },
+
+        save(v) {
+          value = v;
+
+          return null;
+        },
+
+        load() {
+          return value || null;
+        },
+      });
       // implement node event listeners here
     },
   },
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,21 +1,22 @@
-describe('empty spec', () => {
-  beforeEach('passes', () => {
-    cy.visit('https://kitchen.applitools.com/ingredients/iframe')
-  })
+describe("empty spec", () => {
+  beforeEach("passes", () => {
+    cy.visit("https://kitchen.applitools.com/ingredients/iframe");
+  });
 
-  it('Verify iframe',()=>{
+  it("Verify iframe", () => {
     //cy.get('#the-kitchen-table').its('contentDocument.body').then(cy.wrap)
-     // .find('button.ytp-large-play-button').should('be.visible')
-    cy.iframe('#youtube-table-cypress').find('button.ytp-large-play-button',{timeout:60000}).should('be.visible').click()
-  })
+    // .find('button.ytp-large-play-button').should('be.visible')
+    cy.iframe("#youtube-table-cypress")
+      .find("button.ytp-large-play-button", { timeout: 60000 })
+      .should("be.visible")
+      .click();
+  });
 
-  it('Verify iframe TEST 2',()=>{
-    cy.get('#the-kitchen-table').then(($iframe)=>{
-         const iframeContent = $iframe.contents().find('body')
+  it("Verify iframe TEST 2", () => {
+    cy.get("#the-kitchen-table").then(($iframe) => {
+      const iframeContent = $iframe.contents().find("body");
 
-
-         cy.wrap(iframeContent).find('#fruits-vegetables')
-    })
-
-   })
-})
+      cy.wrap(iframeContent).find("#fruits-vegetables");
+    });
+  });
+});

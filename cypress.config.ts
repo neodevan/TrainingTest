95a76c232d810b3a08d657a5bd9095a39a543949
neodevan <itsmevipinmp5@gmail.com>
2022-12-22 00:57:53 +0000

    verify record key

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   projectId: "ss7ape",
   e2e: {
     env: {
-      RECORD_KEY: "690a20b7-73e9-456d-b052-72c5663d1f7a",
+      recordKey: "690a20b7-73e9-456d-b052-72c5663d1f7a",
     },
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {

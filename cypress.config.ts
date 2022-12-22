dcb031f58c083ff4efae983612228ec0a9a6adbc
neodevan <itsmevipinmp5@gmail.com>
2022-12-22 01:56:35 +0000

    Recort Test New

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "ss7ape",
   e2e: {
-    env: {
-      RECORD_KEY: "146dfedc-beec-47ba-a98d-62d3a6e2e5e8",
-    },
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       let value:any;
